4bc29a1506891028ab08929cee57c193ba1505f3
leynier <leynier41@gmail.com>
2020-03-24 18:19:34 -0400

    Separate widget in home page

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,8 +1,5 @@
-import 'dart:developer' as dev;
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import 'package:covid19cuba/src/blocs/blocs.dart';
 import 'package:covid19cuba/src/utils/utils.dart';
@@ -40,68 +37,7 @@ class HomePageState extends State<HomePage> with TickerProviderStateMixin {
           centerTitle: true,
           title: Text(Constants.appName),
         ),
-        drawer: Drawer(
-          elevation: 0,
-          child: ListView(
-            children: <Widget>[
-              DrawerHeader(
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Container(
-                        child: Image.network(
-                          'https://covid19cubadata.github.io/images/logo.jpg',
-                        ),
-                        padding: EdgeInsets.all(5),
-                        width: 100,
-                      ),
-                      Text(
-                        'Covid19 Cuba',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Text(
-                  'Aplicación para conocer los reportes diarios, estadísticas, '
-                  'etc respecto a la Covid-19 en Cuba.',
-                ),
-              ),
-              Container(
-                height: 1,
-                color: Colors.grey,
-                margin: EdgeInsets.symmetric(horizontal: 20),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Text(
-                  'Puede contribuir al desarrollo de esta aplicación, pues '
-                  'es un proyecto de código abierto.',
-                ),
-              ),
-              ListTile(
-                title: Text('Ver el código fuente'),
-                leading: Icon(Icons.link),
-                onTap: () async {
-                  Navigator.pop(context);
-                  const url =
-                      'https://github.com/Covid19-Cuba-Efforts/covid19cuba-app';
-                  if (await canLaunch(url)) {
-                    await launch(url);
-                  } else {
-                    dev.log('Could not launch $url');
-                  }
-                },
-              ),
-            ],
-          ),
-        ),
+        drawer: HomeDrawer(),
         body: FadeTransition(
           opacity: fadeAnimation,
           child: BlocBuilder<HomeBloc, HomeState>(
@@ -115,50 +51,7 @@ class HomePageState extends State<HomePage> with TickerProviderStateMixin {
                 return LoadingWidget();
               }
               if (state is LoadedHomeState) {
-                var data = state.data;
-                return ListView(
-                  children: <Widget>[
-                    Container(
-                      color: Constants.primaryColor,
-                      child: ResumeWidget(data: data),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 5, right: 5, top: 5),
-                      child: Card(
-                        child: EvolutionWidget(data: data),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 5, right: 5, top: 5),
-                      child: Card(
-                        child: DistributionNationalityDiagnosedWidget(
-                          data: data,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 5, right: 5, top: 5),
-                      child: Card(
-                        child: DistributionAgeGroupsDiagnosedWidget(
-                          data: data,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 5, right: 5, top: 5),
-                      child: Card(
-                        child: PieContagionWidget(data: data),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 5, right: 5, top: 5),
-                      child: Card(
-                        child: PieContagionWidget(data: data),
-                      ),
-                    ),
-                    Container(padding: EdgeInsets.all(5)),
-                  ],
-                );
+                return HomeWidget(data: state.data);
               }
               if (state is ErrorHomeState) {
                 return ew.ErrorWidget(errorMessage: state.errorMessage);
--- lib/src/widgets/widgets.dart
+++ lib/src/widgets/widgets.dart
@@ -2,6 +2,8 @@ export 'distribution_age_groups_diagnosed.dart';
 export 'distribution_nationality_diagnosed_widget.dart';
 export 'error_widget.dart';
 export 'evolution_widget.dart';
+export 'home_drawer.dart';
+export 'home_widget.dart';
 export 'loading_widget.dart';
 export 'pie_contagion_widget.dart';
 export 'pie_sex_widget.dart';
