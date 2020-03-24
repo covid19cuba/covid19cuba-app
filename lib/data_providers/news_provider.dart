a56350bf1de808dd7f7e42d11747af3e0af905dd
rmarticedeno <rmarticedeno@gmail.com>
2020-03-24 13:58:02 -0400

    Changed news_provider logic according to NewsModel changes

--- lib/data_providers/news_provider.dart
+++ lib/data_providers/news_provider.dart
@@ -1,26 +1,28 @@
 import 'dart:convert';
 import 'dart:developer';
 import 'dart:async';
+import 'dart:io';
 import 'package:http/http.dart';
 import 'package:covid19cuba/models/models.dart';
 import 'package:covid19cuba/utils/utils.dart';
 
 const news_url = 'http://www.cusobu.nat.cu/covid/telegram/get';
 
-Future<List<NewsModel>> getneswsdata(int date) async {
-    var response = await post(news_url, body: { 'from' : date.toString() });
+Future<NewsModel> getneswsdata(int date) async {
+    var response = await post(news_url, headers: {'Content-Type': 'application/json'} , body: jsonEncode({ 'from' : date.toString() }));
     if(response.statusCode == 404) {
       throw InvalidSourceException('Source is invalid');
     }
     else if (response.statusCode != 200) {
       throw BadRequestException('Bad request');
     }
-    List<NewsModel> result;
+    NewsModel result;
     try {
-      result = NewsModel.newsfromJson(jsonDecode(response.body));
+      result = NewsModel.fromJson(jsonDecode(response.body));
     } catch (error) {
-      log(error);
-      throw ParseException('Parse error');
+      log(error.toString());
+      print(error);
+      //throw ParseException('Parse error');
     }
     return result;
 }
