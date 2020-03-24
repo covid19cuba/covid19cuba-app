0d605862efd4560a67fb5848157326bdccecc664
leynier <leynier41@gmail.com>
2020-03-24 17:53:18 -0400

    Implement constants

---
+++ lib/src/utils/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appName = 'Covid19 Cuba Data';
+  static Color primaryColor = Color.fromARGB(255, 28, 19, 64);
+}
