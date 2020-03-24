ea18fe79a4b904cf7e6e418c6089242daaea6f37
leynier <leynier41@gmail.com>
2020-03-24 05:13:50 -0400

    Implement base for application

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,25 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:preferences/preference_service.dart';
 
 import 'package:covid19cuba/src/app.dart';
 
-void main() => runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+
+  await PrefService.init();
+
+  return runApp(App());
+}
+
+class SimpleBlocDelegate extends BlocDelegate {
+  @override
+  onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    log(transition.toString());
+  }
+}
