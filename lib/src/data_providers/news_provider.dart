1f169920691576539ac27032ad0d488f04f63aa3
leynier <leynier41@gmail.com>
2020-03-26 15:34:19 -0400

    Fix errors

--- lib/src/data_providers/news_provider.dart
+++ lib/src/data_providers/news_provider.dart
@@ -1,28 +1,34 @@
 import 'dart:convert';
 import 'dart:developer';
-import 'dart:async';
-import 'dart:io';
+
 import 'package:http/http.dart';
-import 'package:covid19cuba/models/models.dart';
-import 'package:covid19cuba/utils/utils.dart';
+
+import 'package:covid19cuba/src/models/models.dart';
+import 'package:covid19cuba/src/utils/utils.dart';
 
 const news_url = 'http://www.cusobu.nat.cu/covid/telegram/get';
 
-Future<NewsModel> getneswsdata(int date) async {
-    var response = await post(news_url, headers: {'Content-Type': 'application/json'} , body: jsonEncode({ 'from' : date.toString() }));
-    if(response.statusCode == 404) {
-      throw InvalidSourceException('Source is invalid');
-    }
-    else if (response.statusCode != 200) {
-      throw BadRequestException('Bad request');
-    }
-    NewsModel result;
-    try {
-      result = NewsModel.fromJson(jsonDecode(response.body));
-    } catch (error) {
-      log(error.toString());
-      print(error);
-      //throw ParseException('Parse error');
-    }
-    return result;
+Future<NewsModel> getNewsData(int date) async {
+  var response = await post(
+    news_url,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: jsonEncode({
+      'from': date.toString(),
+    }),
+  );
+  if (response.statusCode == 404) {
+    throw InvalidSourceException('Source is invalid');
+  } else if (response.statusCode != 200) {
+    throw BadRequestException('Bad request');
+  }
+  NewsModel result;
+  try {
+    result = NewsModel.fromJson(jsonDecode(response.body));
+  } catch (e) {
+    log(e.toString());
+    throw ParseException('Parse error');
+  }
+  return result;
 }
--- lib/src/models/news_model.dart
+++ lib/src/models/news_model.dart
@@ -1,54 +1,56 @@
+import 'package:flutter/material.dart';
 
-import 'package:flutter/cupertino.dart';
-
-class SimpleNews{
+class SimpleNews {
   final DateTime date;
   final String provider;
   final String message;
 
-  const SimpleNews({@required this.date, @required this.provider, @required this.message})
-      : assert(date != null),
+  const SimpleNews({
+    @required this.date,
+    @required this.provider,
+    @required this.message,
+  })  : assert(date != null),
         assert(provider != null),
         assert(message != null);
 
-  static SimpleNews fromJson(Map<String, dynamic> json){
+  static SimpleNews fromJson(Map<String, dynamic> json) {
     return SimpleNews(
-        date: new DateTime.fromMicrosecondsSinceEpoch(int.parse(json['timestamp']) * 1000),
-        provider: json['user'],
-        message: json['message']
+      date: DateTime.fromMicrosecondsSinceEpoch(
+        int.parse(json['timestamp']) * 1000,
+      ),
+      provider: json['user'],
+      message: json['message'],
     );
   }
 
-  Map<String,dynamic> toJson(){
-    return <String,dynamic>{
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
       'date': date.toIso8601String(),
       'provider': provider,
-      'message' : message
+      'message': message,
     };
   }
 }
 
-class NewsModel{
+class NewsModel {
   final List<SimpleNews> news;
 
-  const NewsModel({@required this.news})
-      : assert(news != null);
+  const NewsModel({@required this.news}) : assert(news != null);
 
-  static NewsModel fromJson(Map<String, dynamic> json){
-      var newslist = new List<SimpleNews>();
-      if (!json['success']){
-        return null;
-      }
-      for(var key in json['entries']){
-        newslist.add(SimpleNews.fromJson(key));
-      }
-      return NewsModel(news: newslist);
+  static NewsModel fromJson(Map<String, dynamic> json) {
+    var newsList = new List<SimpleNews>();
+    if (!json['success']) {
+      return null;
+    }
+    for (var key in json['entries']) {
+      newsList.add(SimpleNews.fromJson(key));
+    }
+    return NewsModel(news: newsList);
   }
 
-  Map<String,dynamic> toJson(){
-    return <String,dynamic>{
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
       'news': news.map((i) => i.toJson()).toList(),
     };
   }
-
 }
