946a09556b0734713a8774a6a9147ea21c8cef64
rmarticedeno <rmarticedeno@gmail.com>
2020-03-26 21:39:03 -0400

    Changed isInfoUpdated return

--- lib/src/data_providers/info_update_provider.dart
+++ lib/src/data_providers/info_update_provider.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
 import 'dart:developer';
 
+import 'package:covid19cuba/src/models/info_update_model.dart';
 import 'package:http/http.dart';
+
+import 'package:covid19cuba/src/models/models.dart';
 import 'package:covid19cuba/src/utils/utils.dart';
 
 const is_info_updated_url = 'http://www.cusobu.nat.cu/covid/cubadata/status/';
 
-Future<Map<String,dynamic>> isInfoUpdated() async{
+Future<InfoUpdate> isInfoUpdated() async{
   int time = (DateTime.now().millisecondsSinceEpoch / 1000).round() - 1;
   var response = await get(
       is_info_updated_url + time.toString());
@@ -15,12 +18,12 @@ Future<Map<String,dynamic>> isInfoUpdated() async{
   } else if (response.statusCode != 200) {
     throw BadRequestException('Bad request');
   }
-  Map<String,dynamic> json;
+  InfoUpdate update;
   try {
-    json = jsonDecode(response.body);
+    update = InfoUpdate.fromJson(jsonDecode(response.body));
   } catch (e) {
     log(e.toString());
     throw ParseException('Parse error');
   }
-  return json;
+  return update;
 }
