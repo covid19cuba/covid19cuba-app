5b63ee46256dd6c37e13da1d7e4405789c094c58
leynier <leynier41@gmail.com>
2020-03-28 14:51:32 -0400

    Fix bugs in map

--- lib/src/widgets/map_widget.dart
+++ lib/src/widgets/map_widget.dart
@@ -1,63 +1,40 @@
-import 'dart:async';
-import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:covid19cuba/src/utils/utils.dart';
-import 'package:covid19cuba/src/models/data_model.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
-
-import 'dart:developer';
 import 'dart:convert';
 
-String mapdata="{}";
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:getflutter/getflutter.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-/*void LogPrint(Object object) {
-    int defaultPrintLength = 1020;
-    if (object == null || object.toString().length <= defaultPrintLength) {
-       print(object);
-    } else {
-       String log = object.toString();
-       int start = 0;
-       int endIndex = defaultPrintLength;
-       int logLength = log.length;
-       int tmpLogLength = log.length;
-       while (endIndex < logLength) {
-          print(log.substring(start, endIndex));
-          endIndex += defaultPrintLength;
-          start += defaultPrintLength;
-          tmpLogLength -= defaultPrintLength;
-       }
-       if (tmpLogLength > 0) {
-          print(log.substring(start, logLength));
-       }
-    }
+import 'package:covid19cuba/src/utils/utils.dart';
+import 'package:covid19cuba/src/models/data_model.dart';
 
-}*/
+const showMunicipality = "\$('#map-pro').hide();\$('#map-num').show();";
+const showProvince = "\$('#map-mun').hide();\$('#map-pro').show();";
 
-class MapWebViewWidget extends StatelessWidget {
-
+class MapWebViewWidget extends StatefulWidget {
   final DataModel data;
+
   MapWebViewWidget({this.data}) : assert(data != null);
 
-  final Completer<WebViewController> _controller =
-      Completer<WebViewController>();
+  @override
+  MapWebViewWidgetState createState() => MapWebViewWidgetState();
+}
 
+class MapWebViewWidgetState extends State<MapWebViewWidget> {
+  String mapData = "{}";
   WebViewController cont;
-  String selecte_view = 'Municipio';
+  String selectedView = 'Municipio';
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         Container(
-          margin: EdgeInsets.only(
-            left: 20,
-            right: 20,
-            top: 20,
-          ),
+          margin: EdgeInsets.only(left: 20, right: 20, top: 20),
           child: Center(
             child: Text(
-              'Distribución por $selecte_view',
+              'Distribución por $selectedView',
               textAlign: TextAlign.center,
               style: TextStyle(
                 color: Constants.primaryColor,
@@ -69,88 +46,123 @@ class MapWebViewWidget extends StatelessWidget {
         ),
         Container(
           height: 300,
-           margin: EdgeInsets.only(
-            left: 20,
-            right: 20,
-            top: 20,
+          margin: EdgeInsets.only(
+            left: 0,
+            right: 0,
+            top: 0,
           ),
-          // constraints: Constrains,
           child: WebView(
-            initialUrl: 'assets/map.html',
-            javascriptMode: JavascriptMode.unrestricted,
-            onPageFinished: (_) {
-              mapdata=jsonEncode(data.cases.toJson());
-              //LogPrint(mapdata);
-              cont
-                  .evaluateJavascript('covidData($mapdata)')
-                  .whenComplete((){
-                      print('Mapa injected');
-                      cont
-                      .evaluateJavascript("\$('#map-pro').hide();\$('#map-num').show();")
-                      .whenComplete((){
-                        print('Change to province map');
-                        selecte_view = 'Municipio';
+              initialUrl: 'assets/map.html',
+              javascriptMode: JavascriptMode.unrestricted,
+              onPageFinished: (_) {
+                mapData = jsonEncode(widget.data.cases.toJson());
+                cont.evaluateJavascript('covidData($mapData)').whenComplete(
+                  () {
+                    cont.evaluateJavascript(showMunicipality).whenComplete(() {
+                      setState(() {
+                        selectedView = 'Municipio';
                       });
-                    }
-                  );
-            },
-            gestureRecognizers: Set()
+                    });
+                  },
+                );
+              },
+              gestureRecognizers: Set()
                 ..add(
-                    Factory<PanGestureRecognizer>(() => PanGestureRecognizer()))
+                  Factory<PanGestureRecognizer>(
+                    () => PanGestureRecognizer(),
+                  ),
+                )
                 ..add(
                   Factory<VerticalDragGestureRecognizer>(
-                      () => VerticalDragGestureRecognizer()),
+                    () => VerticalDragGestureRecognizer(),
+                  ),
                 )
                 ..add(
                   Factory<HorizontalDragGestureRecognizer>(
-                      () => HorizontalDragGestureRecognizer()),
+                    () => HorizontalDragGestureRecognizer(),
+                  ),
                 )
                 ..add(
                   Factory<ScaleGestureRecognizer>(
-                      () => ScaleGestureRecognizer()),
+                    () => ScaleGestureRecognizer(),
+                  ),
                 ),
-            onWebViewCreated: (WebViewController webViewController) {
-              cont = webViewController;
-          }),
+              onWebViewCreated: (WebViewController webViewController) {
+                cont = webViewController;
+              }),
         ),
         ButtonBar(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              FlatButton(
-                color: Constants.primaryColor,
-                shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-                onPressed: () {
-                  cont
-                        .evaluateJavascript("\$('#map-mun').hide();\$('#map-pro').show();")
-                        .whenComplete(() => print('Change to province map'));
-                  selecte_view = 'Provincia';
-                  (context as Element).markNeedsBuild();
-                },
-                child: Text("Provincia",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            GFButton(
+              text: 'Provincia',
+              textColor: Constants.primaryColor,
+              color: Constants.primaryColor,
+              size: GFSize.LARGE,
+              shape: GFButtonShape.pills,
+              type: GFButtonType.outline2x,
+              fullWidthButton: true,
+              onPressed: () {
+                cont.evaluateJavascript(showProvince);
+                setState(() {
+                  selectedView = 'Provincia';
+                });
+              },
+            ),
+            GFButton(
+              text: 'Municipio',
+              textColor: Constants.primaryColor,
+              color: Constants.primaryColor,
+              size: GFSize.LARGE,
+              shape: GFButtonShape.pills,
+              type: GFButtonType.outline2x,
+              fullWidthButton: true,
+              onPressed: () {
+                cont.evaluateJavascript(showMunicipality);
+                setState(() {
+                  selectedView = 'Municipio';
+                });
+              },
+            ),
+            /*FlatButton(
+              color: Constants.primaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              onPressed: () {
+                cont.evaluateJavascript(showProvince);
+                setState(() {
+                  selectedView = 'Provincia';
+                });
+              },
+              child: Text(
+                'Provincia',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              FlatButton(
-                color: Constants.primaryColor,
-                shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-                onPressed: () {
-                  cont
-                        .evaluateJavascript("\$('#map-pro').hide();\$('#map-mun').show();")
-                        .whenComplete(() => print('Change to municipality map'));
-                  selecte_view = 'Municipio';
-                  (context as Element).markNeedsBuild();
-                },
-                child: Text("Municipio",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
+            ),
+            FlatButton(
+              color: Constants.primaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              onPressed: () {
+                cont.evaluateJavascript(showMunicipality);
+                setState(() {
+                  selectedView = 'Municipio';
+                });
+              },
+              child: Text(
+                'Municipio',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ]
+            ),*/
+          ],
         )
       ],
     );
