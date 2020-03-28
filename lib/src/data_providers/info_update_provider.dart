d475985479319b4cc0d36b3b89082afe8ceef44c
rmarticedeno <rmarticedeno@gmail.com>
2020-03-28 15:44:54 -0400

    Added shared preferences query to info update provider

--- lib/src/data_providers/info_update_provider.dart
+++ lib/src/data_providers/info_update_provider.dart
@@ -5,11 +5,21 @@ import 'package:http/http.dart';
 
 import 'package:covid19cuba/src/models/models.dart';
 import 'package:covid19cuba/src/utils/utils.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 const is_info_updated_url = 'http://www.cusobu.nat.cu/covid/cubadata/status/';
 
 Future<InfoUpdate> isInfoUpdated() async {
-  int time = (DateTime.now().millisecondsSinceEpoch / 1000).round() - 1;
+  int time;
+  SharedPreferences prefs;
+  try{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    time = await prefs.getInt('last_data_update');
+  }
+  catch (e){
+    log(e.toString());
+    time = (DateTime.now().millisecondsSinceEpoch / 1000).round() - 1;
+  }
   var response = await get('$is_info_updated_url$time');
   if (response.statusCode == 404) {
     throw InvalidSourceException('Source is invalid');
