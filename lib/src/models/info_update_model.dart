f94dc28961d231afcf32d9d963c61c995bfe4c02
leynier <leynier41@gmail.com>
2020-03-30 03:25:01 -0400

    Generate parser for InfoUpdate class

--- lib/src/models/info_update_model.dart
+++ lib/src/models/info_update_model.dart
@@ -1,29 +1,32 @@
-import 'package:flutter/material.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'info_update_model.g.dart';
+
+@JsonSerializable()
 class InfoUpdate {
+  @JsonKey(name: 'updated', fromJson: dateTimeFromJson, toJson: dateTimeToJson)
   final DateTime date;
+  @JsonKey(defaultValue: false)
   final bool needUpdate;
 
-  const InfoUpdate({@required this.needUpdate, this.date});
+  const InfoUpdate({this.needUpdate, this.date});
+
+  static DateTime dateTimeFromJson(String x) {
+    return DateTime.fromMillisecondsSinceEpoch(
+      int.parse(x),
+    );
+  }
+
+  static String dateTimeToJson(DateTime dateTime) {
+    return dateTime.toIso8601String();
+  }
+
+  Map<String, dynamic> toJson() => _$InfoUpdateToJson(this);
 
   static InfoUpdate fromJson(Map<String, dynamic> json) {
     if (json['success']) {
-      return InfoUpdate(
-        needUpdate: true,
-        date: DateTime.fromMillisecondsSinceEpoch(
-          int.parse(
-            json['updated'],
-          ),
-        ),
-      );
+      return _$InfoUpdateFromJson(json);
     }
     return InfoUpdate(needUpdate: false);
-  }
-
-  Map<String, dynamic> toJson() {
-    return <String, dynamic>{
-      'updated': date.toIso8601String(),
-      'needUpdate': needUpdate,
-    };
   }
 }
