eb4996b7ee1bfe48a8aaad0fa4b3e72cb2715469
leynier <leynier41@gmail.com>
2020-04-06 19:58:07 -0400

    Remove default country setting, now only in home page

--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -1,30 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:preferences/preferences.dart';
 
-import 'package:covid19cuba/src/models/models.dart';
 import 'package:covid19cuba/src/utils/utils.dart';
 
 class SettingsPage extends StatefulWidget {
-  final DataModel data;
-
-  SettingsPage({Key key, this.data}) : super(key: key) {
-    assert(data != null);
-  }
-
   @override
   State<SettingsPage> createState() => SettingsPageState();
 }
 
 class SettingsPageState extends State<SettingsPage> {
-  List<String> getCountriesList() {
-    var list = widget.data.comparisonOfAccumulatedCases.countries.keys
-        .where((c) => c != Constants.countryCuba)
-        .toList();
-    list.sort((a, b) =>
-        DataModel.prettyCountry(a).compareTo(DataModel.prettyCountry(b)));
-    return list;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,23 +19,6 @@ class SettingsPageState extends State<SettingsPage> {
         centerTitle: true,
       ),
       body: PreferencePage([
-        PreferenceTitle(
-          'País a comparar por defecto:',
-          style: TextStyle(
-            color: Constants.primaryColor,
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
-          ),
-        ),
-        DropdownPreference(
-          'País',
-          Constants.prefCompareCountry,
-          defaultVal: Constants.defaultCompareCountry,
-          values: getCountriesList(),
-          displayValues: getCountriesList()
-              .map((value) => DataModel.prettyCountry(value))
-              .toList(),
-        ),
         PreferenceTitle(
           'Intervalo de actualización:',
           style: TextStyle(
