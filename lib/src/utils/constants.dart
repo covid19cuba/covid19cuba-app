c192dcfcce1400ec84ea74f1478d2d8ebc3a006e
rmarticedeno <rmarticedeno@gmail.com>
2020-04-12 18:44:36 -0400

    Added apkUpdateText constant

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -7,7 +7,7 @@ class Constants {
   static const String diseaseName = 'Covid-19';
   static const Color primaryColor = Color.fromARGB(255, 28, 19, 64);
   static const int notificationMinutesRange = 1440;
-    static const int setUpTasksMinutesDefault = 15;
+  static const int setUpTasksMinutesDefault = 15;
 
   static const String defaultCompareCountry = 'Hungary';
   static const String countryCuba = 'Cuba';
@@ -33,9 +33,16 @@ class Constants {
 
   static Time clapsTime = Time(20, 55, 00);
 
-  static const String apklisUrl = "https://www.apklis.cu/application/club.postdata.covid19cuba";
-  static const String githubUrl = "https://github.com/covid19cuba/covid19cuba-app/releases/latest/download/app.apk";
+  static const String apklisUrl =
+      "https://www.apklis.cu/application/club.postdata.covid19cuba";
+  static const String githubUrl =
+      "https://github.com/covid19cuba/covid19cuba-app/releases/latest/download/app.apk";
 
   static const String apklisBanner = "assets/images/apklis.png";
   static const String githubBanner = "assets/images/github.png";
+
+  static const String apkUpdateText =
+      'Ya esta disponible una nueva actualización de nuestra aplicación. '
+      'Es posible adquirir nuestra nueva actualización desde la red nacional cubana'
+      ' con Akplis o desde internet con Github.';
 }
