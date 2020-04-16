ae06e0c849fd325ede04f6cb609aac1e75c4f38f
rmarticedeno <rmarticedeno@gmail.com>
2020-04-16 09:28:48 -0400

    Added first FAQs

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -72,4 +72,43 @@ class Constants {
     'gtm': 'Guantánamo',
     'ijv': 'Isla de la Juventud',
   };
+
+  static const faqs = <List<String>>[
+    [
+      'Presento un problema con la aplicación que debería hacer ?',
+      'Primero le aconsejamos que lea esta sección de la aplicación y si, su interrogante sigue sin aparecer, '
+          'le invitamos a que entre a nuestro canal y grupo de soporte https://t.me/covid19cubadata y https://t.me/covid19cubadatachat '
+          'respectivamente, para que nuestro equipo le de respuesta a sus preocupaciones.',
+    ],
+    [
+      'Recién se terminó el parte del MINSAP y no se han actualizado los datos. Cuando se hará ?',
+      'Todos nuestros datos son tomados a partir de las partes del Minsap por lo que, tras terminar la conferencia '
+          'nuestro aguerrido equipo de periodistas se toma la tarea de transcribir cada uno de los casos que surgieron en el día. '
+          'Una vez terminado este proceso y se actualicen los datos tenemos un sistema de notificaciones que le alertarán de algún '
+          'cambio en la información. Por ahora presentamos dos fuentes de notificaciones, la primera nuestro bot (asistente virtual) '
+          'presente en Telegram y la segunda esta aplicación (requiere tener instalada una versión superior a la v0.5.0 y tener un '
+          'paquete de datos activo).',
+    ],
+    [
+      'La fecha de los datos que aparece en la aplicación tiene un día de atraso. Significa esto que no se han actualizado los datos?',
+      'No, los partes que se realizan diariamente por el Minsap contienen los casos registrados hasta las 11:59 pm de la noche anterior. '
+          'Por lo que la información que se considera actualizadacon respecto al parte tiene un día de atraso.'
+    ],
+    [
+      'Es posible emplear la aplicación de conjunto con la bolsa nauta ?',
+      'No, desgraciadamente no se dentro de las posibilidades de nuestro equipo de que sea accedible la aplicación por la bolsa nauta. '
+          'Por lo que necesita estar conectado ya sea por wifi o por datos para poder consultar nuestra información.',
+    ],
+    [
+      'La aplicación consume datos nacionales ?',
+      'Si, aunque es posible seleccionar de cual fuente de datos prefiere obtener su información. Si prefiere que consuma primero de '
+          'fuentes nacionales y si, estas fallan consulta en internet (opción por defecto). Todas estas variantes son configurables y '
+          'las puede encontrar en su menú lateral en el apartado de opciones.',
+    ],
+    [
+      'Es posible emplear los datos de los que disponen ?',
+      'Si, es posible emplear los datos que disponemos. Los tenemos disponibles tanto en formato JSON como CSV. para más información '
+          'consulte nuestro links https://covid19cubadata.github.io/ o https://www.cusobu.nat.cu/covid/',
+    ]
+  ];
 }
