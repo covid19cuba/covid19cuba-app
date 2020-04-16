2611ebcabb815abddf3b174ecf1b131d2d2e6511
rmarticedeno <rmarticedeno@gmail.com>
2020-04-16 19:09:07 -0400

    Added App Bar

--- lib/src/pages/faqs_page.dart
+++ lib/src/pages/faqs_page.dart
@@ -2,10 +2,14 @@ import 'package:getflutter/getflutter.dart';
 import 'package:flutter/material.dart';
 import 'package:covid19cuba/src/utils/utils.dart';
 
-class FaqsPage extends StatelessWidget{
+class FaqsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Preguntas Frecuentes"),
+        centerTitle: true,
+      ),
       backgroundColor: Constants.primaryColor,
       body: Center(
         child: ListView(
@@ -15,18 +19,14 @@ class FaqsPage extends StatelessWidget{
     );
   }
 
-  List<Widget> faqs(){
+  List<Widget> faqs() {
     List<Widget> result = [];
-    for(var faq in Constants.faqs){
-      result.add(
-        GFAccordion(
-          title: faq[0],
-          content: faq[1],
-        )
-      );
+    for (var faq in Constants.faqs) {
+      result.add(GFAccordion(
+        title: faq[0],
+        content: faq[1],
+      ));
     }
     return result;
   }
-
-
 }
