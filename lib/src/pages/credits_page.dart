e42e46e1fcfe3fcd92c16f18c0b5da51ad5d8a33
rmarticedeno <rmarticedeno@gmail.com>
2020-04-19 07:55:52 -0400

    Added Credits Page

---
+++ lib/src/pages/credits_page.dart
@@ -0,0 +1,17 @@
+import 'package:getflutter/getflutter.dart';
+import 'package:flutter/material.dart';
+import 'package:covid19cuba/src/utils/utils.dart';
+
+class CreditsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Constants.primaryColor,
+      body: Center(
+        child: ListView(
+          children: [Text("hello")],
+        ),
+      ),
+    );
+  }
+}
