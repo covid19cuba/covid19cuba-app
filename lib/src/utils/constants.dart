b6cf2c8ea74f635c723dc92e8c478b8807998d91
rmarticedeno <rmarticedeno@gmail.com>
2020-05-29 10:55:48 -0400

    Added local storage proxy key names

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -104,6 +104,10 @@ class Constants {
   static const String prefBadgeNews = 'prefBadgeNews';
   static const String prefProxy = 'proxy';
   static const String prefProxyCredentials = 'proxy-authorization';
+  static const String prefProxyHost = 'proxy-host';
+  static const String prefProxyPort = 'proxy-port';
+  static const String prefProxyCredentialsName = 'proxy-credentials-name';
+  static const String prefProxyCredentialsPassword = 'proxi-credentials-password';
 
   //Connection Modes
   static const int ConnectionModeIntranet = 0;
