8dd35dd95212c9f3c667bf307d35075ed9287441
rmarticedeno <rmarticedeno@gmail.com>
2020-05-30 13:00:31 -0400

    Added Disabling feature to proxy setting

--- lib/src/pages/proxy_page.dart
+++ lib/src/pages/proxy_page.dart
@@ -10,12 +10,27 @@ import 'package:preferences/preferences.dart';
 
 import 'package:covid19cuba/src/utils/utils.dart';
 
-class ProxyPage extends StatelessWidget {
+class ProxyPage extends StatefulWidget{
+  @override
+  State<ProxyPage> createState() => ProxyPageState();
+
+}
+
+
+class ProxyPageState extends State<ProxyPage> {
+  bool proxyEnabled;
+  bool credentialsEnabled;
+
   static const divider = const Divider(
     thickness: 1,
     indent: 16,
     endIndent: 16,
   );
+
+  ProxyPageState(){
+    this.proxyEnabled = PrefService.getBool(Constants.prefProxy) ?? false;
+    this.credentialsEnabled = PrefService.getBool(Constants.prefProxyCredentials) ?? false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,10 +53,12 @@ class ProxyPage extends StatelessWidget {
           'Activar Servidor Proxy',
           Constants.prefProxy,
           desc: 'Obtener los datos a través de un servidor proxy',
+          onChange: updateProxySettings,
         ),
         TextFieldPreference(
           'Dirección Web', Constants.prefProxyHost,
           defaultVal: '',
+          disabled: !this.proxyEnabled,
           validator: (String str) {
             if (str.contains(' ')) {
               return "Dirección Inválida";
@@ -53,6 +70,7 @@ class ProxyPage extends StatelessWidget {
           'Puerto',
           Constants.prefProxyPort,
           defaultVal: '',
+          disabled: !this.proxyEnabled,
           validator: (str) {
             try{
               var value = int.parse(str);
@@ -71,14 +89,18 @@ class ProxyPage extends StatelessWidget {
           'Credenciales',
           Constants.prefProxyCredentials,
           desc: 'Credenciales para emplear el servidor proxy',
+          disabled: !this.proxyEnabled,
+          onChange: updateProxyCredentialsSettings,
         ),
         TextFieldPreference(
           'Nombre de Usuario', Constants.prefProxyCredentialsName,
           defaultVal: '',
+          disabled: !this.proxyEnabled || !this.credentialsEnabled,
         ),
         TextFieldPreference(
           'Contraseña',
           Constants.prefProxyCredentialsPassword,
+          disabled: !this.proxyEnabled || !this.credentialsEnabled,
           defaultVal: '',
           obscureText: true,
         ),
@@ -92,9 +114,22 @@ class ProxyPage extends StatelessWidget {
           Constants.prefProxyAllowInsecureCertificates,
           desc: 'Aceptar cualquier certificado de seguridad que sea devuelto al conectarse al proxy. '
               'Esta opción no tiene efecto si la conexión necesita el empleo de credenciales',
+          disabled: !this.proxyEnabled,
         ),
         divider,
       ]),
     );
+  }
+
+  void updateProxySettings(){
+    setState(() {
+      this.proxyEnabled = PrefService.getBool(Constants.prefProxy);
+    });
+  }
+
+  void updateProxyCredentialsSettings(){
+    setState(() {
+      this.credentialsEnabled = PrefService.getBool(Constants.prefProxyCredentials);
+    });
   }
 }
