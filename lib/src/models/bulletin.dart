fe5e5f928f788007f3a40f4be58a06e914abd2a1
rmarticedeno <rmarticedeno@gmail.com>
2020-06-07 01:16:39 -0400

    Added bulletin model

---
+++ lib/src/models/bulletin.dart
@@ -0,0 +1,21 @@
+// Copyright (C) 2020 covid19cuba
+// Use of this source code is governed by a GNU GPL 3 license that can be
+// found in the LICENSE file.
+
+import 'package:json_annotation/json_annotation.dart';
+
+part 'bulletin.g.dart';
+
+@JsonSerializable()
+class Bulletin{
+  String id;
+  String info;
+  String url;
+  int size;
+
+  Bulletin();
+
+  factory Bulletin.fromJson(Map<string, dynamic> json) => _$BulletinFromJson(json);
+
+  Map<String, dynamic> toJson() => _$BulletinToJson(this);
+}
