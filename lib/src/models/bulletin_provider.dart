55524beed79f37e2bcba8224e3d52ba5e691d6e4
rmarticedeno <rmarticedeno@gmail.com>
2020-06-07 01:28:36 -0400

    Added bulletin provider model

---
+++ lib/src/models/bulletin_provider.dart
@@ -0,0 +1,21 @@
+// Copyright (C) 2020 covid19cuba
+// Use of this source code is governed by a GNU GPL 3 license that can be
+// found in the LICENSE file.
+
+import 'package:json_annotation/json_annotation.dart';
+import 'bulletin.dart';
+
+part 'bulletin_provider.g.dart';
+
+class BulletinProvider{
+  String name;
+  String url;
+  String data_source;
+  List<Bulletin> bulletins;
+
+  BulletinProvider();
+
+  factory BulletinProvider.fromJson(Map<String,dynamic> json) => _$BulletinProviderFromJson(json);
+
+  Map<String, dynamic> toJson() => _$BulletinProviderToJson(this);
+}
