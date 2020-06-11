85b941b70090f1edd6f7a2bdd2ca9e84b57495c5
rmarticedeno <rmarticedeno@gmail.com>
2020-06-11 15:25:38 -0400

    Add Bulletin State Model

---
+++ lib/src/models/bulletins_state.g.dart
@@ -0,0 +1,16 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'bulletins_state.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+BulletinState _$BulletinStateFromJson(Map<String, dynamic> json) {
+  return BulletinState()..cache = json['cache'] as String;
+}
+
+Map<String, dynamic> _$BulletinStateToJson(BulletinState instance) =>
+    <String, dynamic>{
+      'cache': instance.cache,
+    };
