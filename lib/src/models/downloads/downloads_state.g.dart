4b32a97742e2a1f55c56fa52830b12e63bb93035
leynier <leynier41@gmail.com>
2020-06-28 01:46:23 -0400

    Implement downloads section

---
+++ lib/src/models/downloads/downloads_state.g.dart
@@ -0,0 +1,16 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'downloads_state.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DownloadsState _$DownloadsStateFromJson(Map<String, dynamic> json) {
+  return DownloadsState()..cache = json['cache'] as String;
+}
+
+Map<String, dynamic> _$DownloadsStateToJson(DownloadsState instance) =>
+    <String, dynamic>{
+      'cache': instance.cache,
+    };
