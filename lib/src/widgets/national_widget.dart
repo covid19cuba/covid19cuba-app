085bdebc97af047d0e74e74ac2a47ff8202e2a36
leynier <leynier41@gmail.com>
2020-07-04 20:08:33 -0400

    Change order of NoteWidget

--- lib/src/widgets/national_widget.dart
+++ lib/src/widgets/national_widget.dart
@@ -16,13 +16,6 @@ class NationalWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        if (shouldBe()) getWidget(context),
-        Container(
-          margin: EdgeInsets.only(left: 5, right: 5, top: 5),
-          child: Card(
-            child: ResumeWidget(resume: data.all.resume),
-          ),
-        ),
         Container(
           margin: data.all.note == null || data.all.note == ''
               ? EdgeInsets.all(0)
@@ -30,6 +23,13 @@ class NationalWidget extends StatelessWidget {
           child: Card(
             color: Colors.red,
             child: NoteWidget(note: data.all.note),
+          ),
+        ),
+        if (shouldBe()) getWidget(context),
+        Container(
+          margin: EdgeInsets.only(left: 5, right: 5, top: 5),
+          child: Card(
+            child: ResumeWidget(resume: data.all.resume),
           ),
         ),
         Container(
